812ec96f71ff6743ec68e3f3a1d647721f0c9b6c
blackdizel <blackdizel@gmail.com>
2017-01-24 19:44:47 +0300

    add android project and assets

---
+++ android/src/org/byters/jamgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.byters.jamgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.byters.jamgame.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
