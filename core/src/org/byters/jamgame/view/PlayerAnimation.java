4b5904c34b8ce939d382069bb4b6e680530f4cb6
blackdizel <blackdizel@gmail.com>
2017-03-14 14:34:26 +0300

    add tint color texture position calc

--- core/src/org/byters/jamgame/view/PlayerAnimation.java
+++ core/src/org/byters/jamgame/view/PlayerAnimation.java
@@ -12,21 +12,22 @@ import org.byters.jamgame.model.DrawableObjectsEnum;
 import org.byters.jamgame.model.PlayerModel;
 
 public class PlayerAnimation {
+    private static final float WATER_HEIGHT_FACTOR = 0.4f;
     private static final String SPRITE_FILE_MOVE = "graphics/walk.png";
     private static final String SPRITE_FILE_INTERACT = "graphics/action.png";
     private static final String SPRITE_FILE_STAND = "graphics/hero.png";
-
     private static final int FRAMES_NUM_MOVE = 4;
     private static final float FRAME_DURATION_SECONDS = 0.09f;
     private static final int FRAMES_NUM_INTERACT = 4;
     private static final int FRAMES_NUM_STAND = 1;
-
     private Animation<TextureRegion> aStand;
     private Animation<TextureRegion> aMove;
     private Animation<TextureRegion> aInteract;
-    private Animation<TextureRegion> aCurrent;
     private TextureRegion currentFrame;
     private AnimationDrawableObject animationDrawableObject;
+    private int frameWidth, frameHeight;
+    private TextureRegion textureTinted;
+    private int heightNotTinted;
 
     public AnimationDrawableObject getDrawableObject() {
         return animationDrawableObject;
@@ -37,22 +38,27 @@ public class PlayerAnimation {
         aMove = initAnimation(SPRITE_FILE_MOVE, FRAMES_NUM_MOVE);
         aInteract = initAnimation(SPRITE_FILE_INTERACT, FRAMES_NUM_INTERACT);
         aStand = initAnimation(SPRITE_FILE_STAND, FRAMES_NUM_STAND);
+
+        textureTinted = new TextureRegion();
     }
 
     private Animation<TextureRegion> initAnimation(String file, int framesNum) {
 
         Texture texture = new Texture(Gdx.files.internal(file));
-        TextureRegion[][] frames = TextureRegion.split(texture, texture.getWidth() / framesNum, texture.getHeight());
-
+        frameWidth = texture.getWidth() / framesNum; //TODO use in one texture for all animations
+        frameHeight = texture.getHeight();
+        TextureRegion[][] frames = TextureRegion.split(texture, frameWidth, frameHeight);
         TextureRegion[] framesMove = new TextureRegion[framesNum];
         for (int i = 0; i < framesNum; ++i)
             framesMove[i] = frames[0][i];
 
-        return new Animation<TextureRegion>(FRAME_DURATION_SECONDS, framesMove);
+        Animation<TextureRegion> result = new Animation<TextureRegion>(FRAME_DURATION_SECONDS, framesMove);
+        result.setPlayMode(Animation.PlayMode.LOOP);
+        return result;
     }
 
     void update() {
-        aCurrent = ControllerPlayer.getInstance().getPlayerAnimationState() == PlayerModel.AnimationState.STAND
+        Animation<TextureRegion> aCurrent = ControllerPlayer.getInstance().getPlayerAnimationState() == PlayerModel.AnimationState.STAND
                 ? aStand
                 : ControllerPlayer.getInstance().getPlayerAnimationState() == PlayerModel.AnimationState.MOVE
                 ? aMove
@@ -61,18 +67,44 @@ public class PlayerAnimation {
                 : null;
 
         if (aCurrent == null) return;
-        currentFrame = aCurrent.getKeyFrame(
-                ControllerMain.getInstance().getGameTime() / 1000f % aCurrent.getAnimationDuration(), true);
 
-        if (!ControllerPlayer.getInstance().isPlayerDirectionRight() && !currentFrame.isFlipX())
-            currentFrame.flip(true, false);
-        else if (ControllerPlayer.getInstance().isPlayerDirectionRight() && currentFrame.isFlipX())
-            currentFrame.flip(true, false);
+        float stateTime = ControllerMain.getInstance().getGameTime() / 1000f;
+        int keyFrameIndex = aCurrent.getKeyFrameIndex(stateTime);
+        currentFrame = aCurrent.getKeyFrames()[keyFrameIndex];
+
+        updateTintHeight();
+        currentFrame.setRegion(keyFrameIndex * frameWidth, 0, frameWidth, Math.min(heightNotTinted, frameHeight));
+
+        checkFlip(currentFrame);
+
+        if (heightNotTinted >= frameHeight) {
+            textureTinted.setTexture(null);
+        } else {
+            textureTinted.setTexture(currentFrame.getTexture());
+            textureTinted.setRegion(keyFrameIndex * frameWidth, heightNotTinted, frameWidth, frameHeight - heightNotTinted);
+            checkFlip(textureTinted);
+        }
+    }
+
+    private void updateTintHeight() {
+        heightNotTinted = (int) (ControllerPlayer.getInstance().getPlayerPositionY() + frameHeight - Gdx.graphics.getHeight() * WATER_HEIGHT_FACTOR);
+    }
+
+    private void checkFlip(TextureRegion textureRegion) {
+        if (!ControllerPlayer.getInstance().isPlayerDirectionRight() && !textureRegion.isFlipX()
+                || ControllerPlayer.getInstance().isPlayerDirectionRight() && textureRegion.isFlipX())
+            textureRegion.flip(true, false);
     }
 
     @Nullable
-    public TextureRegion getTexture() {
+    TextureRegion getTexture() {
         return currentFrame;
+    }
+
+    @Nullable
+    TextureRegion getTextureTinted() {
+        if (textureTinted.getTexture() == null) return null;
+        return textureTinted;
     }
 
     public class AnimationDrawableObject implements IDrawableObject {
@@ -84,7 +116,7 @@ public class PlayerAnimation {
 
         @Override
         public int getY() {
-            return (int) ControllerPlayer.getInstance().getPlayerPositionY();
+            return (int) ControllerPlayer.getInstance().getPlayerPositionY() + Math.max(0, frameHeight - heightNotTinted);
         }
 
         @Override
